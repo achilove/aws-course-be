bb6fc22b8a7d353bea1fd8d4ab5d3643ea857808
Javokhir Achilov <javokhir_achilov@epam.com>
2021-06-13 23:26:19 +0500

    feat: add products endpoints

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# aws-course-be
-Here will be Back-end for tasks related to nodejs in aws course
+endpoints:
+  GET - https://33j0f1vd50.execute-api.eu-west-1.amazonaws.com/dev/products
+  GET - https://33j0f1vd50.execute-api.eu-west-1.amazonaws.com/dev/products/{id}
+
+FE pull request with products endpoints integrated
+https://github.com/rolling-scopes-school/nodejs-aws-fe/pull/288
---
+++ product-service/helper.js
@@ -0,0 +1,31 @@
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+};
+
+const response = (body, statusCode) => {
+    body = JSON.stringify(body);
+    return {
+        statusCode,
+        body,
+        headers
+    };
+}
+
+module.exports.successResponse = body => {
+    return response(body, 200);
+}
+
+module.exports.notFoundResponse = item => {
+    const message = item + ' not found';
+    return response({
+        message
+    }, 400);
+}
+
+module.exports.unexpectedErrorResponse = () => {
+    const message = 'Unexpected error has occurred';
+    return response({
+        message
+    }, 500);
+}
