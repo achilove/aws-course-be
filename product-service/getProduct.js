1c6da767957b12416d134e4bc408e5e6f4740aa7
Javokhir <32628703+achilove@users.noreply.github.com>
2021-07-23 10:58:10 +0500

    feat: retrieve product from db

--- product-service/getProduct.js
+++ product-service/getProduct.js
@@ -1,15 +1,18 @@
-const products = require('products-mock');
 const { successResponse, notFoundResponse, unexpectedErrorResponse } = require('helper');
+const { Client } = require('pg');
+const { dbOptions } = require('./db/config');
 
-module.exports = async (event) => {
+module.exports = async ( event ) => {
+    const client = new Client(dbOptions);
+    await client.connect();
     try {
         const { pathParameters } = event;
         const { id } = pathParameters;
-        const product = products.filter(elem => elem.id === id)[0];
-        if( !product ){
+        const { rows: products } = await client.query(`select products.id, products.title, products.price, products.description, stocks.count from products left join stocks on products.id = stocks.product_id where products.id=${id}`);
+        if( !products[0] ){
             return notFoundResponse('product');
         }
-        return successResponse(product);
+        return successResponse(products[0]);
     }catch(e){
         return unexpectedErrorResponse('product');
     }
