441a18b95de56bd0470701f79a4dc64310e0c804
Javokhir <32628703+achilove@users.noreply.github.com>
2021-08-12 22:30:27 +0500

    fix: use parameterized queries

--- product-service/getProduct.js
+++ product-service/getProduct.js
@@ -10,7 +10,7 @@ module.exports = async ( event ) => {
         console.log('getProduct handler input:', event);
         const { pathParameters } = event;
         const { id } = pathParameters;
-        const { rows: products } = await client.query(`select products.id, products.title, products.price, products.description, stocks.count from products left join stocks on products.id = stocks.product_id where products.id='${id}';`);
+        const { rows: products } = await client.query(`select products.id, products.title, products.price, products.description, stocks.count from products left join stocks on products.id = stocks.product_id where products.id=$1`, [id]);
         if( !products[0] ){
             return notFoundResponse('product');
         }
